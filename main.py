1e10afc37552d0e97ed3b4955a370eec655eb640
Akshay Nehe <akshaadnehe@gmail.com>
2020-03-14 17:43:39 -0400

    Replaced dict generation with set representation, more Pythonic way

--- main.py
+++ main.py
@@ -7,29 +7,17 @@ __purpose__ = "Check if a one-to-one character mapping can exist from string1 to
 
 def compare_strings(str1, str2):
     """
-    Dictionaries for unique characters of each string are being generated.
+    Both strings are represented as strings.
     Core logic is to check whether unique characters in str1 are more than or equal to
     unique characters in str2.
     """
-    len1 = len(str1)
-    dict1 = dict()
-    len2 = len(str2)
-    dict2 = dict()
 
-    larger_length = max(len1, len2)
-
-    for idx in range(larger_length):
-        if idx < len1:
-            dict1[str1[idx]] = dict1.get(str1[idx], 0) + 1
-        elif len(dict2.keys()) > len(dict1.keys()):
-            break
-        if idx < len2:
-            dict2[str2[idx]] = dict2.get(str2[idx], 0) + 1
-        elif len(dict2.keys()) <= len(dict1.keys()):
-            break
-
-    result = 'true' if len(dict2.keys()) <= len(dict1.keys()) else 'false'
-    print(result)
+    set1 = set(str1)
+    set2 = set(str2)
+    if len(set1) >= len(set2):
+        print('true')
+    else:
+        print('false')
 
 
 if __name__ == '__main__':
